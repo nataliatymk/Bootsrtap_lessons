9d5222f8eefe30499bd398f034bf8a9f9e9b83c5
nataliatymk <nataliatymkow@op.pl>
2020-05-12 23:42:30 +0200

    second_part

--- exercise/urls.py
+++ exercise/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from exercise import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
