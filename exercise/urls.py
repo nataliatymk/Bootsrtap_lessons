625d16cad12024b1f1db30e91b55d7d17dbc6228
nataliatymk <nataliatymkow@op.pl>
2020-05-13 00:17:45 +0200

    third_part_difrent_way_to_make_views

--- exercise/urls.py
+++ exercise/urls.py
@@ -5,8 +5,10 @@ from exercise import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
